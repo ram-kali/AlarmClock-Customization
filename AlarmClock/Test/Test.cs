9af7451df344e68567854dbdde13170a596f3d6b
Ramakaliappan K <ramkaliappank@gmail.com>
2025-01-21 11:29:47 +0530

    Implement POM framework with base class and generic keyword methods
    Implemented POM Framework with
    - Base Class
    - Generic Keyword
    - Test Class (main method)

---
+++ AlarmClock/Test/Test.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp1.TestCases;
+using ConsoleApp1.Utilities;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+class Test : BaseClass
+{
+    public static void Main()
+    {
+        beforeAll();
+        if (true)
+        {
+            //
+        }
+
+    }
+}
